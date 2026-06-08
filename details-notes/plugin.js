b7c39d6bb4b8f21e2a586e247ea9754054871e71
Lea Verou <lea@verou.me>
2026-06-08 09:19:39 -0400

    Initialize `<details class=notes>` lazily when the slide is shown

--- details-notes/plugin.js
+++ details-notes/plugin.js
@@ -3,22 +3,42 @@ import { create } from "@inspirejs/core/util";
 
 export const hasCSS = true;
 
+const css = new URL("./plugin.css", import.meta.url).href;
+
+Inspire.hooks.add("slidechange", ({ slide, firstTime }) => {
+	let details = slide.querySelector("details.notes");
+
+	if (!details) {
+		return;
+	}
+
+	if (firstTime) {
+		let div = document.createElement("div");
+		div.append(...details.childNodes);
+		details.append(div);
+		details.classList.add("docs-icons"); // for docs plugin
+
+		if (!/\b(top|bottom)-(right|left)\b/.test(details.className)) {
+			details.classList.add("top-right");
+		}
+
+		let summary = details.querySelector("summary");
+
+		if (!summary) {
+			details.insertAdjacentHTML("afterbegin", "<summary>Notes</summary>");
+		}
+
+		// let shadowRoot = slide.shadowRoot || slide.attachShadow({ mode: "open" });
+		// shadowRoot.append(details);
+		// shadowRoot.adoptedStyleSheets.push(create(css));
+	}
+
+	if (Inspire.projector) {
+		details.open = true;
+	}
+});
+
 for (let details of document.querySelectorAll("details.notes")) {
-	let div = document.createElement("div");
-	div.append(...details.childNodes);
-	details.append(div);
-	details.classList.add("docs-icons"); // for docs plugin
-
-	if (!/\b(top|bottom)-(right|left)\b/.test(details.className)) {
-		details.classList.add("top-right");
-	}
-
-	let summary = details.querySelector("summary");
-
-	if (!summary) {
-		create.start(details, `<summary>Notes</summary>`);
-	}
-
 	if (Inspire.projector) {
 		// Speaker view, let's have the notes open by default
 		details.open = true;
